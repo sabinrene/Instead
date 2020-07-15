afd1b806d8560b018799b238d8da69f39027e703
Tidus <Tidus@hotmail.com>
2020-07-15 05:11:45 -0500

    Night

--- Pages/Courses/LiveDetail.php
+++ Pages/Courses/LiveDetail.php
@@ -8,12 +8,15 @@
     <link rel="stylesheet" href="../../Public/CSS/Course/LiveDetail.css">
     <link rel="stylesheet" href="../../Public/CSS/Header.css">
     <link rel="stylesheet" href="../../Public/CSS/Footer.css">
+    <link rel="stylesheet" href="../../Public/CSS/Instead.css">
     <link rel="stylesheet" type="text/css" href="../../Public/CSS/Trainee/TraineeMenu.css">
     <link rel="stylesheet" type="text/css" href="../../Public/CSS/Modals.css">
   </head>
 
-<body>
 
+
+
+<body>
 
 <?php include ("../../Pages/Header.php"); ?>
 <?php include ("../../Pages/Trainee/TraineeMenu.php"); ?>
@@ -21,155 +24,139 @@
 <input class="hide" type="text" id="logIn2" value="../../">
 <input id="page" class="hide" type="text" name="" value="">
 
+<!-- Banner -->
+<div class="Banner">
+    <div class="Banner-Txt"> <a href="../../index.php">Smart Plant 3D</a></div>
+</div>
+<!-- END Banner -->
 
 
 
-    <div class="Banner">
-        <div class="Banner-Txt"> <a href="../../index.php">Smart Plant 3D</a></div>
-    </div>
+<!-- Live-SecWrap -->
+<div class="Live-SecWrap">
+
+    <div class="Live-TwoCols clearfix">
 
 
+      <!-- Left Section -->
+        <div class="Live-SecL">
+            <div class="CourseTitle"> <h2 id="courseTitle">  </h2> </div>
+
+            <div id="description" class="Description">
+
+              <div class="BoxBot-Desc">
+                  <h2> SP3D - Piping Course </h2>
+
+                  <p> Smart Plant 3D (SP3D) is a modeling software used in
+                    the engineering sector for pipe designing. This SP3D
+                    training builds the skills required for executing detailed
+                    designing projects in power plants, petrochemical setups,
+                    oil and gas industry, and food and beverage manufacturing
+                    units etc. </p>
+
+                <div class="DescLists">
+                  <h3> Common Introduction: </h3>
+                  <ul>
+                    <li> SmartPlant 3D Introduction </li>
+                    <li> Templates/Session Files </li>
+                    <li> Filters (Database query) </li>
+                    <li> Views </li>
+                    <li> Common Toolbar </li>
+                    <li> Selecting Objects </li>
+                    <li> Space management </li>
+                    <li> Interference Detection </li>
+                  </ul>
+                </div> <!-- Part ONE -->
 
 
-    <div class="Live-SecWrap">
+                <div class="DescLists">
+                  <h3> Piping: </h3>
+                  <ul>
+                    <li> Piping Hierarchy </li>
+                    <li> Templates/Session Files </li>
+                    <li> Filters (Database query) </li>
+                    <li> Views </li>
+                    <li> Common Toolbar </li>
+                    <li> Selecting Objects </li>
+                    <li> Space management </li>
+                    <li> Interference Detection </li>
+                  </ul>
+                </div> <!-- Part TWO -->
 
-        <div class="Live-TwoCols clearfix">
-
-            <div class="Live-SecL">
-                <div class="CourseTitle"> <h2 id="courseTitle">  </h2> </div>
-
-                <div id="description" class="Description">
-
-                  <div class="BoxBot-Desc">
-                      <h2> SP3D - Piping Course </h2>
-
-                      <p> Smart Plant 3D (SP3D) is a modeling software used in
-                        the engineering sector for pipe designing. This SP3D
-                        training builds the skills required for executing detailed
-                        designing projects in power plants, petrochemical setups,
-                        oil and gas industry, and food and beverage manufacturing
-                        units etc. </p>
-
-                  <div class="PartOne">
-                    <h3> Common Introduction: </h3>
-                    <ul>
-                      <li> SmartPlant 3D Introduction </li>
-                      <li> Templates/Session Files </li>
-                      <li> Filters (Database query) </li>
-                      <li> Views </li>
-                      <li> Common Toolbar </li>
-                      <li> Selecting Objects </li>
-                      <li> Space management </li>
-                      <li> Interference Detection </li>
-                    </ul>
-                  </div>
-
-                  <div class="PartTwo">
-                    <h3> Piping: </h3>
-                    <ul>
-                      <li> Piping Hierarchy </li>
-                      <li> Templates/Session Files </li>
-                      <li> Filters (Database query) </li>
-                      <li> Views </li>
-                      <li> Common Toolbar </li>
-                      <li> Selecting Objects </li>
-                      <li> Space management </li>
-                      <li> Interference Detection </li>
-                    </ul>
-                  </div>
-
-                  </div>
+              </div> <!-- END BoxBot Description -->
 
 
-                    <div class="Live-Box">
-                            <div class="Price"> <h2 id="price"> $1250 </h2> </div>
-                            <div class="BoxBot">
-
-
-                                <div class="BoxBot-Txt">
-                                    <p> <span> Online Classes: </span> 40 hours </p>
-                                    <p> <span> Online Training: </span> 20 hours </p>
-                                    <p> <span> Tools: </span> Smart Plant 3D </p>
-                                    <p> <span> Capacity: </span> 10 Students </p>
-
-
-                                    <a href="https://us02web.zoom.us/j/83844504152?pwd=SnUyZHZYTk1jOFpjK0wvT0Z4NW9zQT09
-">
-                                    <button class="PBtn"> Live </button>
-                                    </a>
-
-                                </div>
-
-                            </div>
-                    </div> <!-- END Live-Box -->
-
-
-                </div>
-            </div>
+            </div>  <!-- END Description -->
+        </div>
+        <!-- END Live-SecL-->
 
 
 
 
-            <!-- Live Section Right -->
-            <div class="Live-SecR">
+        <!-- Live Section Right -->
+        <div class="Live-SecR">
 
+        		<div class="CourseTemp-Right-Container">
 
-            		<div class="CourseTemp-Right-Container">
+        			<div>
+        				<a href="#"><img id="image" class="Course-Pic" src="" alt="">
+                  <!-- Esto lo puse YO ahora -->
+                  <img src="../../Public/images/Course-Sample-Pics/06-Autocad.jpg" class="Course-Pic">
+                  <!-- ESTO arriba Aelna -->
+                </a>
+        			</div>
 
-            			<div>
-            				<a href="#"><img id="image" class="Course-Pic" src="" alt=""></a>
-            			</div>
+        			<div id="price" class="Course-Price"> <h1> $5000 </h1> </div>
 
-            			<div id="price" class="Course-Price"> 5000 </div>
+        			<div class="Course-Btns">
+        					<div id="addToCard" class="Btn02 colorBlack">
+        					Add to Cart
+        				  </div>
+        				<div id="buyCourse" class="Btn02">
+        					Buy Now
+        				</div>
+        			</div>
 
-            			<div class="Course-Btns">
-            					<div id="addToCard" class="Btn02 colorBlack">
-            					Add to Cart
-            				  </div>
-            				<div id="buyCourse" class="Btn02">
-            					Buy Now
-            				</div>
-            			</div>
+        			<div class="Course-includes">
+        				<p>This course includes: </p>
+        					<ul>
+                    <li> Live Lectures  </li>
+        						<li> Expert Training </li>
+          					<li> Mobile friendly Access </li>
+        					</ul>
+        			</div>
 
-            			<div class="Course-includes">
-            				<p>This course includes: </p>
-            					<ul>
-            						<li> On-demand video lectures </li>
-            						<li> Articles to deepen knowledge </li>
-            						<li> Mobile friendly Access </li>
-            					</ul>
-            			</div>
+        			<div class="ribbon-wrapper">
+                <a href="CourseMain.php">
+            				<div class="glow">&nbsp;</div>
+              				<div class="ribbon-front">
+              					Course
+              				</div>
+          					<div class="ribbon-edge-topleft"></div>
+          					<div class="ribbon-edge-topright"></div>
+          					<div class="ribbon-edge-bottomleft"></div>
+          					<div class="ribbon-edge-bottomright"></div>
+        			   </a>
+               </div>
 
-            			<div class="ribbon-wrapper">
-                    <a href="CourseMain.php">
-                				<div class="glow">&nbsp;</div>
-                  				<div class="ribbon-front">
-                  					Course
-                  				</div>
-              					<div class="ribbon-edge-topleft"></div>
-              					<div class="ribbon-edge-topright"></div>
-              					<div class="ribbon-edge-bottomleft"></div>
-              					<div class="ribbon-edge-bottomright"></div>
-            			   </a>
-                </div>
+        		</div> <!-- END CourseTemp Right Container -->
 
-            		</div>
-
-
-            </div> <!-- END Live Section Right -->
-        </div> <!-- END Live Two Columns -->
-
+        </div> <!-- END Live Section Right -->
 
     </div>
+    <!-- END Live Two Columns -->
+
+
+</div>
+<!-- END Wrap -->
 
 
 
 
-    <?php include '../../Pages/Footer.php' ?>
 
-    <script src="../../Public/JS/Course/LiveDetail.js"></script>
-    <script src="../../Public/JS/CRUD.js"></script>
-
+<?php include '../../Pages/Footer.php' ?>
+<script src="../../Public/JS/Course/LiveDetail.js"></script>
+<script src="../../Public/JS/CRUD.js"></script>
 
 </body>
 </html>
