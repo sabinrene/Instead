1e8764d284ae44c997d1fe6db460f0ef454676eb
Alejandra Rozo <aelnajard@Alejandras-MacBook-Air.local>
2020-07-22 18:55:07 -0500

    ...

--- Pages/Courses/CourseTemplate.php
+++ Pages/Courses/CourseTemplate.php
@@ -235,7 +235,7 @@
 					</ul>
 			</div>
 
-			<div class="ribbon-wrapper"><a href="CourseMain.php">
+			<div class="ribbon-wrapper"><a href="">
 				<div class="glow">&nbsp;</div>
 				<div class="ribbon-front">
 					Course
