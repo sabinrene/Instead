539b6fbdd0be2cfb252300d2ad826949885c5031
Tidus <Tidus@hotmail.com>
2020-08-01 03:06:29 -0500

    3am

--- Pages/AboutUs.php
+++ Pages/AboutUs.php
@@ -8,17 +8,20 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="../Public/JS/jquery-3.5.0.min.js"></script>
-<link rel="stylesheet" type="text/css" href="../Public/CSS/AboutUs.css">
+
 <link rel="stylesheet" href="../Public/CSS/Header.css">
+
+<link rel="stylesheet" type="text/css" href="../Public/CSS/AboutUs.css">
 <link rel="stylesheet" type="text/css" href="../Public/CSS/Instead.css">
 <link rel="stylesheet" type="text/css" href="../Public/CSS/Footer.css">
+
 <link rel="stylesheet" href="../Public/CSS/Trainee/TraineeMenu.css">
 <link rel="stylesheet" type="text/css" href="../Public/CSS/Modals.css">
 
 
 <!-- Font Awesome + Favicon -->
 <script src="https://kit.fontawesome.com/d2e9898f47.js" crossorigin="anonymous">    </script>
-<link rel="icon" type="image/svg+xml" href="../../Public/images/Instead-favicon.png" sizes="any">
+<link rel="icon" type="image/svg+xml" href="../Public/images/Instead-favicon.png" sizes="any">
 
 </head>
 
@@ -33,87 +36,202 @@
 
 
 
-
-
-
-
-
 <!-- CourseTemp-->
 <div class="CTop">
-    <div class="Top-Title"> About Us </div>
+    <div class="Top-Title"> <h1> About Us </h1> </div>
 </div>
 
 
 
 <div class="Template-Body clearfix">
 
-      <!-- Content -->
-      <div class="ContentArea">
+  <!-- Content -->
+  <div class="ContentArea">
 
-          <div class="Cont-1">
-              <h1> What is Instead? </h1>
-              <p> Ulysta is a brainchild of first generation entrepreneur from
-                Germany and India to provide specialized Consulting and
-                Engineering services to the various industries like Oil and Gas
-                and Power. Through our strategic partnership, we bring
-                extremely high quality consulting and engineering services and
-                dedicated to pleasing clients by giving them what they
-                want before the competitors. </p>
+      <div class="Cont-1">
+          <h1> What is Instead? </h1>
+          <p> Ulysta is a brainchild of first generation entrepreneur from
+            Germany and India to provide specialized Consulting and
+            Engineering services to the various industries like Oil and Gas
+            and Power. Through our strategic partnership, we bring
+            extremely high quality consulting and engineering services and
+            dedicated to pleasing clients by giving them what they
+            want before the competitors. </p>
 
-              <p> Ulysta offering homogeneous blend of engineering services and
-                consulting solutions for various business needs of Oil & Gas
-                Owner/Operators/Process Licensor’s/EPC’s & Engineering
-                companies for the complete Plant Project Life Cycle. </p>
-
-          </div>
-
-          <div class="Cont-2">
-            <h1> Our Mission </h1>
-              <ul>
-                <li> 1. Integrity to all aspects of business, Committed, Empowered and Technically Capable People, Delivering Profitable Sustainability </li>
-                <li> 2. Comprehensive worldwide presence, Global expertise delivery, Responsive to customer preferences, Optimum customized solutions </li>
-                <li> 3. A trusted supplier, partner and customer, Collaborative approach to business, Willing to challenge and innovate, Seeking enduring customer relationships </li>
-              </ul>
-          </div>
-
-          <div class="Cont-3">
-              Section 3
-          </div>
-
-          <div class="Cont-4 clearfix">
-
-                <h1> Our Latest Courses </h1>
-
-                                    <div class="Cont-Card Pic1">
-                                          <h3> Smart Plant 3D - Piping </h3>
-                                    </div>
-
-                                    <div class="Cont-Card Pic2">
-                                        <h3> Smart Plant 3D - P&IDs </h3>
-                                    </div>
-
-                                    <div class="Cont-Card Pic2">
-                                        <h3> Solidworks </h3>
-                                    </div>
-
-                                    <div class="Cont-Card Pic1">
-                                          <h3> Smart Plant 3D - Piping </h3>
-                                    </div>
-
-                                    <div class="Cont-Card Pic2">
-                                        <h3> Smart Plant 3D - P&IDs </h3>
-                                    </div>
-
-                                    <div class="Cont-Card Pic2">
-                                        <h3> Solidworks </h3>
-                                    </div>
-
-          </div>
-
-
+          <p> Ulysta offering homogeneous blend of engineering services and
+            consulting solutions for various business needs of Oil & Gas
+            Owner/Operators/Process Licensor’s/EPC’s & Engineering
+            companies for the complete Plant Project Life Cycle. </p>
 
       </div>
-      <!-- END CourseArea -->
+      <!-- END Cont 1 -->
+
+
+      <!-- Cont 2 -->
+      <div class="Cont-2">
+        <h1> Our Mission </h1>
+          <ul>
+            <li> 1. Integrity to all aspects of business, Committed, Empowered and Technically Capable People, Delivering Profitable Sustainability </li>
+            <li> 2. Comprehensive worldwide presence, Global expertise delivery, Responsive to customer preferences, Optimum customized solutions </li>
+            <li> 3. A trusted supplier, partner and customer, Collaborative approach to business, Willing to challenge and innovate, Seeking enduring customer relationships </li>
+          </ul>
+      </div>
+      <!-- END Cont 2 -->
+
+
+      <!-- Team -->
+      <div class="Team clearfix">
+            <h1> Our Team </h1>
+
+            <div class="RowAlpha">
+
+                <div class="Lead">
+
+                  <div class="LeadPic" id="Jairo"></div>
+                  <div class="LeadInfo">
+                    <h2> Jairo Fernandez </h2>
+                    <h3> Director Colombia </h3>
+                    <p> +30 years of broad international experience
+                      with major engineering & construction companies
+                      during the construction and operation of Oil & Gas,
+                      Power Generation, Mining Facilities and Industrial
+                      Facilities in United States and Latin America.
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                              <a href="#/"><i class="fa fa-instagram"></i></a>
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+
+                <div class="Lead">
+                  <div class="LeadPic" id="Shani"></div>
+                  <div class="LeadInfo">
+                    <h2> Shani Rajpal </h2>
+                    <h3> Director India </h3>
+                    <p> A result oriented professional offering distinguished and qualitative
+                      experience across engineering design and IT engineering, business
+                      administration, corporate marketing and technical positioning.
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                              <a href="#/"><i class="fa fa-instagram"></i></a>
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+            </div>
+
+
+            <!-- BETA -->
+            <div class="RowBeta clearfix">
+
+                <div class="Neoph">
+                  <div class="LeadPic" id="Jules"></div>
+                  <div class="LeadInfo">
+                    <h2> Julian Orozco </h2>
+                    <h3> Industrial IoT Architect  </h3>
+                    <p> Nuclear Engineer focused on IoT with 12 years of broad
+                      International Experience in Operations of Nuclear Reactors
+                      and Development of Interconnected Systems.
+
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+
+                <div class="Neoph">
+                  <div class="LeadPic" id="Aelna"></div>
+                  <div class="LeadInfo">
+                    <h2> Alejandra Rozo </h2>
+                    <h3> Software Developer </h3>
+                    <p> Glavi amet ritnisl libero molestie ante ut
+                      fringilla purus eros quis glavrid from dolor amet
+                      iquam lorem bibendum
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                              <a href="#/"><i class="fa fa-instagram"></i></a>
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+
+
+                <div class="Neoph-in">
+                  <div class="LeadPic" id="Vipul"></div>
+                  <div class="LeadInfo">
+                    <h2> Vipul Tyagi </h2>
+                    <h3> Operations Head Solar </h3>
+                    <p> With an experience of more than 7 years, Vipul has handled
+                      several projects in energy domain ranging from Oil and
+                      Gas to Solar power. He is responsible for the operations
+                      of solar projects and ensures smooth timely delivery.
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                              <a href="#/"><i class="fa fa-instagram"></i></a>
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+
+                <div class="Neoph-in">
+                  <div class="LeadPic" id="Gaurav"></div>
+                  <div class="LeadInfo">
+                    <h2> Gaurav Tiwari </h2>
+                    <h3> GM Projects </h3>
+                    <p> Having been endorsed several start-ups and corporations
+                      for futuristic growth in Indian energy division and also
+                      being associated with different NGO’s, Gaurav’s dynamic
+                      nature, skills and vision can sketch plans into reality.
+                    </p>
+
+                      <div class="SocialMedia">
+                              <a href="#"><i class="fa fa-facebook"></i></a>
+                              <a href="#"><i class="fa fa-twitter"></i></a>
+                              <a href="#/"><i class="fa fa-instagram"></i></a>
+                              <a href="#/"><i class="fa fa-linkedin"></i></a>
+                      </div>
+
+                  </div>
+                </div>
+
+
+            </div> <!-- END RowBETA -->
+
+      </div>
+    <!-- END Team -->
+
+
+
+
+  </div>
+  <!-- END CourseArea -->
+
+
 </div>
 
 
